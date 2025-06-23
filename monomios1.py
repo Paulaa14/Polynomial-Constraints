9f3a81b13de656474f385af4f2f107f48ab8cb6b
Paulaa14 <paula.peiro.castello@gmail.com>
2025-06-23 13:13:34 +0200

    Restricciones para casos en los que no hay que combinar varias variables intermedias para generar una nueva

--- monomios1.py
+++ monomios1.py
@@ -5,14 +5,23 @@
 # Primero como un problema SAT, puedo resolverlo con 2 variables intermedias? 3? Teniendo como grado máximo permitido 2 por ejemplo.
 # Aparecen muchas simetrías de soluciones que solamente se diferencian en el renombrado de las variables. Se pueden controlar numerando u ordenando.
 
-from collections import defaultdict
+from collections import Counter, defaultdict
 import itertools
 
+def addsum(a):
+    if len(a) == 0:
+        return 0
+    elif len(a) == 1:
+        return a[0]
+    else :
+        x = a.pop()
+        return x + addsum(a) 
 
 def expand_factors(factors): # [x_1, x_1, x_2]
     expanded = []
+
     for f in factors:
-        var = f"x_{f['signal']}"
+        var = 'x_' + str(f["signal"])
         expanded.extend([var] * f["degree"])
     return expanded
 
@@ -31,12 +40,17 @@ def count_combinations(monomios, maxDeg):
     mapa = defaultdict(int) # Cada subexpresión en cuántos monomios aparece
 
     for monomio in monomios:
+        
         combs = generate_combinations(monomio["factors"], maxDeg)
 
         for c in (combs):
             mapa[c] += 1 # Sumas 1 en las subexpresiones que están en el monomio
 
     return mapa
+
+# Una lista está contenida dentro de otra
+def contiene_variables(variables, target):
+    return not (Counter(target) - Counter(variables))
 
 import json
 from z3 import *
@@ -61,46 +75,31 @@ maxDeg = 2 # Luego leerlo del fichero
 num_intermedias = 3 # Número máximo de variables intermedias que se permiten
 num_monomios = len(monomios)
 
+degrees = [] # Cada monomio qué grado tiene
+
+for i in range(num_monomios):
+    # Ordenar por el valor de signal para que xy == yx
+    monomios[i]["factors"] = sorted(monomios[i]["factors"], key=lambda f: f["signal"])
+
+    degrees.append(0)
+    for f in monomios[i]["factors"]:
+        degrees[i] = degrees[i] + f["degree"]
+
+
 # Todas las posibles subexpresiones que aparecen en los monomios y el número de veces que aparece cada una
 combinaciones = count_combinations(monomios, maxDeg)
 
 print(combinaciones)
 
-# Procesado de la entrada
+# Ordeno de manera descendiente por el segundo valor que indica el número de veces que aparece cada combinación
+# combinaciones = sorted(combinaciones.items(), key=lambda item: item[1], reverse=True)
 
-# lista_variables = [] # Variables que se tienen para añadir todas las combinaciones
-# combinaciones = map() # Mapa para cada subexpresión cuántas veces aparece
-
-# for m in (num_monomios):
-#     factores = monomios[m]["factors"]
-
-#     for f in (len(factores)): # Cada señal
-
-#         indice = factores["signal"]
-#         señal = "x_" + indice # x1, x2...
-
-#         # if not(señal in lista_variables):
-#         #    lista_variables.append(señal)
-#         for a in range(min(factores["degree"], maxDeg)): # Añadir x1, x1^2, x1^3...., x1^maxDeg
-#             x = ""
-#             for i in range(a):
-#                 x += señal
-#             combinaciones[x] = combinaciones[x] + 1 # Hay una nueva aparición de dicha subexpresión
-
-        # Para toda variable dentro del monomio se añade la combinación con ella
-#         for s in (len(factores)):
-#             if indice != factores[s]:
-
-    
-    
-
-solver = Solver()
+solver = Optimize()
 
 # Tener una lista con las posibles combinaciones de variables y las que más conviene quedarse son las que más aparecen
 
 # Variables Z3
 grados_monomios = [] # Para cada monomio, qué grado tiene. Ya sea originalmente o tras hacer alguna sustitución.
-grados_intermedias = [] # Cada variable intermedia, qué grado tiene. Elegir de las combinaciones, cuáles coges y cuáles no.
 keeps = [] # Como máximo puede haber num_intermedias VI, pero puede haber menos. Obligo a que estén las primeras en el array
 # Otra opción es tener un array del mismo tamaño que el array de las combinaciones de las variables con los booleanos 
 # para cada una y el número de keeps debe ser <= num_intermedias
@@ -110,14 +109,40 @@ for i in range(num_monomios):
 
     solver.add(grados_monomios[i] >= 0)
     solver.add(grados_monomios[i] <= maxDeg)
+    # solver.add(grados_monomios[i] <= degrees[i]) # El grado tiene que ser menor o igual que el grado original
 
-for i in range(num_intermedias):
-    grados_intermedias.append(Int("degi_" + str(i)))
+combinaciones_lista = list(combinaciones.keys())
+
+k = []
+for i in range(len(combinaciones)):
     keeps.append(Bool("keep_" + str(i)))
 
-    solver.add(grados_intermedias[i] >= 0)
-    solver.add(grados_intermedias[i] <= maxDeg)
+    solver.add_soft(Not(keeps[i]), 1, id = "keeps") # Minimiza el número de variables intermedias
+    solver.add_soft(keeps[i], combinaciones[combinaciones_lista[i]], id = "apariciones") # Hace que se prioricen las VI que aparecen más veces
 
-for i in range(num_intermedias - 1): 
-    # Si keeps[i] está a falso, todas las demás tienen que estarlo, así obligo a que se quede las primeras variables
-    solver.add(Or(keeps[i], Not(keeps[i + 1])))
+    k.append(If(keeps[i], 1, 0))
+
+solver.add(addsum(k) <= num_intermedias) # El número de variables intermedias debe ser menor o igual que el valor máximo permitido
+
+for i in range(num_monomios):
+    deg = []
+    f = expand_factors(monomios[i]["factors"])
+    for j in range(len(combinaciones_lista)):
+        if contiene_variables(f, list(combinaciones_lista[j])):
+            deg.append(If(keeps[j], len(combinaciones_lista[j]) - 1, 0))
+    solver.add(grados_monomios[i] == degrees[i] - addsum(deg))
+
+
+if solver.check() == sat:
+    m = solver.model()
+
+    for i in range(len(keeps)):
+        if is_true(m.eval(keeps[i])):
+            combo = combinaciones_lista[i]
+            nombre_variable = "*".join(combo) # Convierte la tupla ('x_1', 'x_2') a "x_1*x_2"
+            print(f"Variable intermedia seleccionada: {nombre_variable}")
+
+else: print("unsat")
+
+# print(solver.assertions())
+# print(solver.model())
