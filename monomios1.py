b107b1338e50a106517d2bc2b1a334dffdc0c73e
Paulaa14 <paula.peiro.castello@gmail.com>
2025-06-23 16:59:36 +0200

    Añadida la opción de tener más de un polinomio como suma de monomios y funciona OK

--- monomios1.py
+++ monomios1.py
@@ -27,7 +27,7 @@ def expand_factors(factors): # [x_1, x_1, x_2]
 
 def generate_combinations(factors, maxDeg):
     expanded = expand_factors(factors)
-    combinaciones = set() # Conjunto para que no se repitan los conjuntos
+    combinaciones = set() # Conjunto para que no se repitan los elementos
 
     for r in range(1, min(maxDeg, len(expanded)) + 1): # Para que solo saque expresiones como mucho de maxDeg, el resto no me sirven como variables intermedias
         # combinations(p, r) -> tuplas de longitud r ordenadas y no repetidas de los elementos en p
@@ -43,7 +43,7 @@ def count_combinations(monomios, maxDeg):
         
         combs = generate_combinations(monomio["factors"], maxDeg)
 
-        for c in (combs):
+        for c in combs:
             mapa[c] += 1 # Sumas 1 en las subexpresiones que están en el monomio
 
     return mapa
@@ -70,33 +70,47 @@ data = json.load(f)
 
 file = open(args.fileout, "w")
 
-monomios = data["monomials"]
-maxDeg = 2 # Luego leerlo del fichero
+polinomios = data["polinomials"]
+num_polinomios = data["num"]
+maxDeg = data["degree"] #2 # Luego leerlo del fichero
 num_intermedias = 3 # Número máximo de variables intermedias que se permiten
-num_monomios = len(monomios)
 
 degrees = [] # Cada monomio qué grado tiene
+num_monomios = 0
+combinaciones = defaultdict(int)
 
-for i in range(num_monomios):
-    # Ordenar por el valor de signal para que xy == yx
-    monomios[i]["factors"] = sorted(monomios[i]["factors"], key=lambda f: f["signal"])
+count = 0
+for p in range(num_polinomios):
+    monomios = polinomios[p]["monomials"]
+    num_monomios += len(monomios)
 
-    degrees.append(0)
-    for f in monomios[i]["factors"]:
-        degrees[i] = degrees[i] + f["degree"]
+    for i in range(len(monomios)):
+        # Ordenar por el valor de signal para que xy == yx
+        monomios[i]["factors"] = sorted(monomios[i]["factors"], key=lambda f: f["signal"])
 
+        degrees.append(0)
+        for f in monomios[i]["factors"]: # Al ser un producto se suma
+            degrees[count] = degrees[count] + f["degree"]
 
-# Todas las posibles subexpresiones que aparecen en los monomios y el número de veces que aparece cada una
-combinaciones = count_combinations(monomios, maxDeg)
+        count = count + 1
+
+    # Todas las posibles subexpresiones que aparecen en los monomios y el número de veces que aparece cada una
+    cb = count_combinations(monomios, maxDeg)
+
+    for c in cb:
+        combinaciones[c] += cb[c]
+
 
 print(combinaciones)
 
-# Ordeno de manera descendiente por el segundo valor que indica el número de veces que aparece cada combinación
-# combinaciones = sorted(combinaciones.items(), key=lambda item: item[1], reverse=True)
+combinaciones_lista = list(combinaciones.keys()) # Claves del mapa, posibles variables intermedias
+
+VI = {}
+for k in range(len(combinaciones_lista)):
+    clave = f"e{k}"
+    VI[clave] = combinaciones_lista[k]
 
 solver = Optimize()
-
-# Tener una lista con las posibles combinaciones de variables y las que más conviene quedarse son las que más aparecen
 
 # Variables Z3
 grados_monomios = [] # Para cada monomio, qué grado tiene. Ya sea originalmente o tras hacer alguna sustitución.
@@ -111,26 +125,28 @@ for i in range(num_monomios):
     solver.add(grados_monomios[i] <= maxDeg)
     # solver.add(grados_monomios[i] <= degrees[i]) # El grado tiene que ser menor o igual que el grado original
 
-combinaciones_lista = list(combinaciones.keys())
-
-k = []
+# k = []
 for i in range(len(combinaciones)):
     keeps.append(Bool("keep_" + str(i)))
 
     solver.add_soft(Not(keeps[i]), 1, id = "keeps") # Minimiza el número de variables intermedias
     solver.add_soft(keeps[i], combinaciones[combinaciones_lista[i]], id = "apariciones") # Hace que se prioricen las VI que aparecen más veces
 
-    k.append(If(keeps[i], 1, 0))
+    # k.append(If(keeps[i], 1, 0))
 
-solver.add(addsum(k) <= num_intermedias) # El número de variables intermedias debe ser menor o igual que el valor máximo permitido
+# solver.add(addsum(k) <= num_intermedias) # El número de variables intermedias debe ser menor o igual que el valor máximo permitido
 
-for i in range(num_monomios):
-    deg = []
-    f = expand_factors(monomios[i]["factors"])
-    for j in range(len(combinaciones_lista)):
-        if contiene_variables(f, list(combinaciones_lista[j])):
-            deg.append(If(keeps[j], len(combinaciones_lista[j]) - 1, 0))
-    solver.add(grados_monomios[i] == degrees[i] - addsum(deg))
+# Para cada monomio de cada polinomio, ver qué variables intermedias contiene
+for p in range(num_polinomios):
+    monomios = polinomios[p]["monomials"]
+    for i in range(len(monomios)):
+        deg = []
+        f = expand_factors(monomios[i]["factors"])
+
+        for j in range(len(combinaciones_lista)):
+            if contiene_variables(f, list(combinaciones_lista[j])):
+                deg.append(If(keeps[j], len(combinaciones_lista[j]) - 1, 0))
+        solver.add(grados_monomios[i] == degrees[i] - addsum(deg))
 
 
 if solver.check() == sat:
@@ -146,4 +162,3 @@ else: print("unsat")
 
 
 # print(solver.assertions())
-# print(solver.model())
