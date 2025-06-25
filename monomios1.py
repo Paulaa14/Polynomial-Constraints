0a60f0c4fa76c3646c0de1f2b488b6ca4de540b3
Paulaa14 <paula.peiro.castello@gmail.com>
2025-06-25 12:27:16 +0200

    Primera versión a falta de hacer optimizaciones y más pruebas funciona medianamente :)

--- monomios1.py
+++ monomios1.py
@@ -14,8 +14,8 @@ def addsum(a):
         return a[0]
     else :
         x = a.pop()
-        return x + addsum(a) 
-    
+        return x + addsum(a)
+
 def addexists(a):
     if len(a) == 0:
         return False
@@ -23,7 +23,7 @@ def addexists(a):
         return a[0]
     else :
         x = a.pop()
-        return Or(x,addexists(a)) 
+        return Or(x,addexists(a))
 
 def expand_factors(factors): # [x_1, x_1, x_2]
     expanded = []
@@ -41,7 +41,7 @@ def generate_combinations(factors, maxDeg):
         # combinations(p, r) -> tuplas de longitud r ordenadas y no repetidas de los elementos en p
         for combo in itertools.combinations(expanded, r):
             combinaciones.add(combo)
-    
+
     return combinaciones
 
 def count_combinations(monomios, maxDeg):
@@ -50,7 +50,7 @@ def count_combinations(monomios, maxDeg):
     for monomio in monomios:
         combs = generate_combinations(monomio["factors"], maxDeg)
         for c in combs:
-            combinaciones.add(tuple(sorted(c)))
+            if len(c) > 1: combinaciones.add(tuple(sorted(c)))
     return combinaciones
 
 # Una lista está contenida dentro de otra
@@ -111,115 +111,106 @@ for p in range(num_polinomios):
 
 
 print(combinaciones)
-
-# combinaciones_lista = list(combinaciones.keys()) # Claves del mapa, posibles variables intermedias
-
-# VI = {} # Igual sirve para las combinaciones de variables originales para crear variables nuevas
-# for k in range(len(combinaciones_lista)):
-#     clave = f"e{k}"
-#     VI[clave] = combinaciones_lista[k]
-
+if mayor_grado_polinomio <= maxDeg:
+    sys.exit("No es necesario añadir ninguna variable auxiliar.")
 
 solver = Optimize()
 
 grados_monomios = [] # Para cada monomio, qué grado tiene. Ya sea originalmente o tras hacer alguna sustitución.
-keeps = [] # Array de tamaño el número de combinaciones de las variables con un booleano para cada una de ellas
 
 for i in range(num_monomios):
     grados_monomios.append(Int("degm_" + str(i)))
 
     solver.add(grados_monomios[i] >= 0)
     solver.add(grados_monomios[i] <= maxDeg)
-    # solver.add(grados_monomios[i] <= degrees[i]) # El grado tiene que ser menor o igual que el grado original
 
-# k = []
-for i in range(len(combinaciones)):
-    keeps.append(Bool("keep_" + str(i)))
+# Con los factores de nivel 0 puedes escoger cualquiera, las variables intermedias solo puedes utilizar las que estén activas
+# Booleanos para ver qué monomios ya tienen grado <= maxDeg. Cuando todos estén a cierto se para de buscar variables y de subir niveles
 
-    solver.add_soft(Not(keeps[i]), 1, id = "keeps") # Minimiza el número de variables intermedias
-    # solver.add_soft(keeps[i], combinaciones[combinaciones[i]], id = "apariciones") # Hace que se prioricen las VI que aparecen más veces
+expresiones = set() # Indexando obtienes la expresión i-ésima para luego reconstruir recursivamente
+activas = []
 
-    # k.append(If(keeps[i], 1, 0))
+num_expresiones = len(combinaciones)
+lista_combinaciones = list(combinaciones)
 
-# solver.add(addsum(k) <= num_intermedias) # El número de variables intermedias debe ser menor o igual que el valor máximo permitido
+for i in range(num_expresiones):
+    expresiones.add((i, )) # Añadir x1 como VI no tiene sentido
 
-# Para cada monomio de cada polinomio, ver qué variables intermedias contiene
+# Como máximo vas a tener que meter tantas variables como el mayor grado dentro del polinomio. SE PUEDE ACOTAR MÁS
+# Primero se construyen todas las posibilidades. Otra opción es hacerlo dinámicamente
+for i in range(int(mayor_grado_polinomio.bit_length())): # AJUSTAR. Coge log2(maxGrado)
+    for r in range(2, maxDeg + 1):
+        # Coge todas las anteriores
+        for combo in itertools.product(range(num_expresiones), repeat = r): # Así combina e1*e1... en vez de x1*x1
+            expresiones.add(tuple(sorted(combo)))
+
+    num_expresiones = len(expresiones)
+
+expresiones = sorted(expresiones, key=lambda x: (x[0], x[1] if len(x) > 1 else -1))
+lista_expresiones = list(expresiones) # Lista de tuplas
+
+for i in range(len(lista_expresiones)):
+    activas.append(Bool("act_" + str(i)))
+
+    solver.add_soft(Not(activas[i]), 1, id = "activas") # Minimiza el número de variables intermedias
+
+# E5 = E1*E1. Solo se puede usar E5 si se usa también E1. Recursivamente se va haciendo
+for i in range(len(lista_expresiones)):
+    if i >= len(combinaciones): # No es una expresión del nivel 0
+        for exp in lista_expresiones[i]:
+            solver.add(Implies(activas[i], activas[exp]))
+
+def reconstruir_expresiones(id, reconstrucciones):
+    if id < len(reconstrucciones): # Ya está calculado
+        return reconstrucciones[id]
+    else:
+        expresion = []
+        for e in lista_expresiones[id]:
+            expr = reconstruir_expresiones(e, reconstrucciones)
+            for elem in expr: expresion.append(elem)
+        return expresion
+
+# Reconstruir variables intermedias creadas
+reconstrucciones = []
+for i in range(len(lista_expresiones)):
+    if i < len(combinaciones): reconstrucciones.append(list(lista_combinaciones[i]))
+    else: 
+        expr = reconstruir_expresiones(i, reconstrucciones)
+        reconstrucciones.append(expr) # Poco eficiente porque igual reconstruyes cosas que no son necesarias
+
+grados = []
 for p in range(num_polinomios):
     monomios = polinomios[p]["monomials"]
     for i in range(len(monomios)):
         deg = []
         f = expand_factors(monomios[i]["factors"])
 
-        # Sólo si la contiene y el keep está a true entonces el grado se modifica
-        for j, combo in enumerate(list(combinaciones)):
-            if contains(f, list(combo)):
-                deg.append(If(keeps[j], len(list(combo)) - 1, 0))
-        solver.add(grados_monomios[i] == degrees[i] - addsum(deg))
+        # Sólo si la contiene y el keep está a true entonces el grado se modifica. 
+        # Recorro al revés para sustituir primero las expresiones más grandes
+        for e in range(len(reconstrucciones) - 1, -1, -1):
+            if contains(f, reconstrucciones[e]):
+                deg.append(If(activas[e], len(reconstrucciones[e]) - 1, 0))
 
+                # Elimino los elementos de f porque ya han sido sustituidos
+                for item in reconstrucciones[e]:
+                    f.remove(item)
 
-# Con los factores de nivel 0 puedes escoger cualquiera, las variables intermedias solo puedes utilizar las que estén activas
-# Booleanos para ver qué monomios ya tienen grado <= maxDeg. Cuando todos estén a cierto se para de buscar variables y de subir niveles
-
-# Matriz, para cada nivel, qué variables tienes
-
-no_cumple_grado = []
-
-for m in range(num_monomios):
-    no_cumple_grado.append(Bool("cumple_" + str(i)))
-
-    # solver.add(no_cumple_grado[m] == grados_monomios[m] > maxDeg) # Vale true si el grado del monomio es mayor que el grado máximo permitido
-
-# niveles = []
-# niveles.append(list(combinaciones)) # El nivel 0 contiene todos los factores del vector combinaciones
-
-expresiones = [] # Indexando obtienes la expresión i-ésima para luego reconstruir recursivamente
-activas = []
-
-num_expresiones_por_nivel = [] # Acumulado
-num_expresiones_por_nivel.append(len(combinaciones))
-
-# Como máximo vas a tener que meter tantas variables como el mayor grado dentro del polinomio. SE PUEDE ACOTAR MUCHO MÁS
-# Primero se construyen todas las posibilidades. Otra opción es hacerlo dinámicamente
-for i in range(mayor_grado_polinomio/2): # AJUSTAR
-    expr = num_expresiones_por_nivel[i]
-    # nivel_i = []
-
-    for r in range(2, maxDeg + 1):
-        for combo in itertools.combinations(range(num_expresiones_por_nivel[i]), r): # Así combina e1*e1... en vez de x1*x1
-            expr += 1 # Una nueva expresión en este nivel
-            # nivel_i.append(combo)
-            expresiones.append(combo)
-    
-    num_expresiones_por_nivel.append(expr)
-
-for i in range(len(expresiones)):
-    activas.append(Bool("act_" + str(i)))
-
-    if i > len(combinaciones): # No es una expresión del nivel 0
-        for e in expresiones[i]:
-            solver.add(Implies(activas[i]), activas[e]) # E5 = E1*E1. Solo se puede usar E5 si se usa también E1. Recursivamente se va haciendo
-
-    
-    solver.add_soft(Not(activas[i]), 1, id = "activas") # Minimiza el número de variables intermedias
-
-
-# Creamos la condición: al menos un monomio no cumple el grado
-# hay_alguno_no_cumple = addexists(no_cumple_grado)
-
-# while solver.check(hay_alguno_no_cumple) == sat: # Queda algún monomio de grado mayor que maxDeg
-#     for nivel in range(niveles, -1, -1): # Bucle al revés
-
+        grados.append(deg)
+        
+for i in range(num_monomios):
+    solver.add(grados_monomios[i] == degrees[i] - addsum(grados[i]))
 
 
 if solver.check() == sat:
     m = solver.model()
 
-    for i in range(len(keeps)):
-        if is_true(m.eval(keeps[i])):
-            combo = list(combinaciones)[i]
+    for i in range(len(activas)):
+        if is_true(m.eval(activas[i])):
+            combo = reconstrucciones[i]
             nombre_variable = "*".join(combo) # Convierte la tupla ('x_1', 'x_2') a "x_1*x_2"
             print(f"Variable intermedia seleccionada: {nombre_variable}")
 
 else: print("unsat")
 
 # print(solver.assertions())
+# file.write(solver.to_smt2())
